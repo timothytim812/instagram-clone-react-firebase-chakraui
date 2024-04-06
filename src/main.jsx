7511d9a1ddd6d30ca941a8292ec57715e3a3a1bf
timothytim812 <timothygeorgebenjamindas@gmail.com>
2024-04-06 14:42:00 +0530

    Edited App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider} from "@chakra-ui/react";
 import theme from "./assets/config/theme.js";
 
 
