2adf12f38094930ee68dbd0e0a679c54180799c9
timothytim812 <timothygeorgebenjamindas@gmail.com>
2024-04-06 14:09:40 +0530

    OptismiZed Comments

--- src/assets/General/Comment.jsx
+++ src/assets/General/Comment.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Flex, Skeleton, SkeletonCircle, Text } from "@chakra-ui/react";
 import useGetUserOfComment from "../components/custom/useGetUserOfComment";
 import { Link } from "react-router-dom";
-import { CreatedAtTimeConversion} from "../utils/CreatedAtTimeConversion";
+import { CreatedAtTimeConversion } from "../utils/CreatedAtTimeConversion";
 
 const Comment = ({ comment }) => {
   const { isLoading, commentedUser } = useGetUserOfComment(comment.createdBy);
@@ -21,23 +21,27 @@ const Comment = ({ comment }) => {
   return (
     <>
       <Flex gap={4}>
-			<Link to={`/${commentedUser.username}`}>
-				<Avatar src={commentedUser.profilePicURL} size={"sm"} />
-			</Link>
-			<Flex direction={"column"}>
-				<Flex gap={2} alignItems={"center"}>
-					<Link to={`/${commentedUser.username}`}>
-						<Text fontWeight={"bold"} fontSize={12}>
-							{commentedUser.username}
-						</Text>
-					</Link>
-					<Text fontSize={14}>{comment.comment}</Text>
-				</Flex>
-				<Text fontSize={12} color={"gray"}>
-					{CreatedAtTimeConversion(comment.createdAt)}
-				</Text>
-			</Flex>
-		</Flex>
+        <Link to={`/${commentedUser.username}`}>
+          <Avatar src={commentedUser.profilePicURL} size={"sm"} />
+        </Link>
+        <Flex direction={"column"} alignItems={"flex-start"}>
+          <Flex gap={2} alignItems={"Flex-start"} justifyContent={"center"}>
+            <Flex flexDirection={"column"}>
+            <Link to={`/${commentedUser.username}`}>
+              <Text fontWeight={"bold"} fontSize={12}>
+                {commentedUser.username}
+              </Text>
+            </Link>
+            <Text fontSize={12} color={"gray"}>
+            {CreatedAtTimeConversion(comment.createdAt)}
+          </Text>
+            </Flex>
+            <Flex justifyContent={"center"} alignItems={"flex-start"}>
+              <Text fontSize={14}>{comment.comment}</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   );
 };
